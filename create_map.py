80506f3148f516b230bd2d3cb389faac11b49011
Joachim Herb <Joachim.Herb@gmx.de>
2013-12-02 21:24:35 +0100

    Move more methods into SatelliteData class

--- create_map.py
+++ create_map.py
@@ -22,82 +22,6 @@ def mkdir_p(path):
             pass
         else: raise
 
-def findStartIndex(img):
-    """Return the first row index not containing the white border at the top"""
-
-    look  = False
-    start = 3
-    i = start
-    left  = 10
-    right = img.shape[0] - 10
-    for r in img[start:]:
-        m = np.amin(r[left:right])
-#        print m
-        if (look and m < 255):
-            return i
-        elif (not look and m == 255):
-            look = True
-        i = i + 1
-    return 0
-
-def cut_borders(data):
-    """Remove the white border of a satellite images (including text)"""
-
-    x1 = findStartIndex(data)
-    data = data[x1:,:]
-    data = data[::-1]
-    x2 = findStartIndex(data)
-    data = data[x2:,:]
-    data = data[::-1]
-
-    data = data.T
-    x1 = findStartIndex(data)
-    data = data[x1:,:]
-    data = data[::-1]
-    x2 = findStartIndex(data)
-    data = data[x2:,:]
-    data = data[::-1]
-    return data.T
-
-def project(satellite):
-    """Reproject the satellite image on an equirectangular map"""
-    
-    img = Image.open(satellite.filename).convert("L")
-    data = np.array(img)
-    data = cut_borders(data)
-
-    x_size = data.shape[1]
-    y_size = data.shape[0]
-    proj_dict = {'a': '6378169.0', 'b': '6356584.0',
-                 'lon_0': satellite.longitude,
-                 'y_0': satellite.false_y,
-                 'h': '35785831.0', 'proj': 'geos'}
-    area_extent =(-satellite.extent, -satellite.extent,
-                  satellite.extent, satellite.extent)
-    area = geometry.AreaDefinition('geo', 'geostat', 'geo', proj_dict, x_size,
-                                   y_size, area_extent)
-    dataIC = image.ImageContainerQuick(data, area)
-
-    proj_dict = {'proj': 'eqc'}
-    area_extent =(-20037508.34, -10018754.17, 20037508.34, 10018754.17)
-    x_size = 2048
-    y_size = 1024
-    pc = geometry.AreaDefinition('pc', 'Plate Carree world map', 'pc',
-                                 proj_dict, x_size, y_size, area_extent)
-
-    #msg_con_nn = image.ImageContainerNearest(data, area, radius_of_influence=50000)
-    dataResampled = dataIC.resample(pc)
-    dataResampledImage = satellite.rescale(dataResampled.image_data)
-
-    dataResampledImage[0:90, :] = dataResampledImage[180:90:-1, :]
-    dataResampledImage[1024-90:1024, :] = dataResampledImage[1024-90:1024-180:-1, :]
-
-    width = 55
-    weight = np.array([max((width - min([abs(satellite.longitude - x),
-                                         abs(satellite.longitude - x + 360),
-                                         abs(satellite.longitude - x - 360)]))/180,
-                           1e-7) for x in np.linspace(-180, 180, dataResampled.shape[1])])
-    return np.array([dataResampledImage, np.tile(weight, (dataResampled.shape[0], 1))])
 
 def saveImage(new_image, filename, overlays = False):
     """Save satellite image"""
@@ -118,14 +42,6 @@ def saveImage(new_image, filename, overlays = False):
 
     img.save(filename)
 
-def curve(b):
-    """Rescale the brightness values used for MTSAT2 satellite"""
-    return np.minimum(b*255.0/193.0, 255)
-
-def ID(b):
-    """Identity function"""
-    return b
-
 class SatelliteData:
     def __init__(self, longitude, extent, false_y, rescale, base_url, suffix):
         self.longitude = longitude
@@ -134,10 +50,21 @@ class SatelliteData:
         self.rescale   = rescale
         self.base_url  = base_url
         self.suffix    = suffix
+        self.rescale   = rescale
         
     def login(self, username, password):
         self.username = username
         self.password = password
+
+    @staticmethod
+    def curve(b):
+        """Rescale the brightness values used for MTSAT2 satellite"""
+        return np.minimum(b*255.0/193.0, 255)
+    
+    @staticmethod
+    def ID(b):
+        """Identity function"""
+        return b
 
     def set_time(self, dt, tempdir=""):
         self.dt = datetime.datetime(dt.year, dt.month, dt.day, dt.hour/3*3, 0, 0)
@@ -166,30 +93,106 @@ class SatelliteData:
         i = Image.open(StringIO(r.content))
         i.save(self.filename)
          
+    def findStartIndex(self, img):
+        """Return the first row index not containing the white border at the top"""
+    
+        look  = False
+        start = 3
+        i = start
+        left  = 10
+        right = img.shape[0] - 10
+        for r in img[start:]:
+            m = np.amin(r[left:right])
+    #        print m
+            if (look and m < 255):
+                return i
+            elif (not look and m == 255):
+                look = True
+            i = i + 1
+        return 0
+    
+    def cut_borders(self):
+        """Remove the white border of a satellite images (including text)"""
+    
+        x1 = self.findStartIndex(self.data)
+        self.data = self.data[x1:,:]
+        self.data = self.data[::-1]
+        x2 = self.findStartIndex(self.data)
+        self.data = self.data[x2:,:]
+        self.data = self.data[::-1]
+    
+        self.data = self.data.T
+        x1 = self.findStartIndex(self.data)
+        self.data = self.data[x1:,:]
+        self.data= self.data[::-1]
+        x2 = self.findStartIndex(self.data)
+        self.data = self.data[x2:,:]
+        self.data = self.data[::-1]
+        return self.data.T
+    
+    def project(self):
+        """Reproject the satellite image on an equirectangular map"""
+        
+        img = Image.open(self.filename).convert("L")
+        self.data = np.array(img)
+        self.data = self.cut_borders()
+    
+        x_size = self.data.shape[1]
+        y_size = self.data.shape[0]
+        proj_dict = {'a': '6378169.0', 'b': '6356584.0',
+                     'lon_0': self.longitude,
+                     'y_0': self.false_y,
+                     'h': '35785831.0', 'proj': 'geos'}
+        area_extent =(-self.extent, -self.extent,
+                      self.extent, self.extent)
+        area = geometry.AreaDefinition('geo', 'geostat', 'geo', proj_dict, x_size,
+                                       y_size, area_extent)
+        dataIC = image.ImageContainerQuick(self.data, area)
+    
+        proj_dict = {'proj': 'eqc'}
+        area_extent =(-20037508.34, -10018754.17, 20037508.34, 10018754.17)
+        x_size = 2048
+        y_size = 1024
+        pc = geometry.AreaDefinition('pc', 'Plate Carree world map', 'pc',
+                                     proj_dict, x_size, y_size, area_extent)
+    
+        #msg_con_nn = image.ImageContainerNearest(data, area, radius_of_influence=50000)
+        dataResampled = dataIC.resample(pc)
+        dataResampledImage = satellite.rescale(dataResampled.image_data)
+    
+        dataResampledImage[0:90, :] = dataResampledImage[180:90:-1, :]
+        dataResampledImage[1024-90:1024, :] = dataResampledImage[1024-90:1024-180:-1, :]
+    
+        width = 55
+        weight = np.array([max((width - min([abs(self.longitude - x),
+                                             abs(self.longitude - x + 360),
+                                             abs(self.longitude - x - 360)]))/180,
+                               1e-7) for x in np.linspace(-180, 180, dataResampled.shape[1])])
+        return np.array([dataResampledImage, np.tile(weight, (dataResampled.shape[0], 1))])
 
 satellite_list = (
                   SatelliteData(145.0, 5433878.562*1.01, 50000,
-                                curve,
+                                SatelliteData.curve,
                                 "http://www.sat.dundee.ac.uk/xrit/145.0E/MTSAT/",
                                 "_MTSAT2_4_S2.jpeg"
                                 ),
                   SatelliteData(57.0, 5568742.4*0.97, 0,
-                                ID,
+                                SatelliteData.ID,
                                 "http://www.sat.dundee.ac.uk/xrit/057.0E/MET/",
                                 "_MET7_2_S2.jpeg"
                                 ),
                   SatelliteData(0.0, 5433878.562, 0,
-                                ID,
+                                SatelliteData.ID,
                                 "http://www.sat.dundee.ac.uk/xrit/000.0E/MSG/",
                                 "_MSG3_9_S2.jpeg"
                                 ),
                   SatelliteData(-75.0, 5433878.562, 0,
-                                ID,
+                                SatelliteData.ID,
                                 "http://www.sat.dundee.ac.uk/xrit/075.0W/GOES/",
                                 "_GOES13_4_S2.jpeg"
                                 ),
                   SatelliteData(-135.0, 5433878.562, 0,
-                                ID,
+                                SatelliteData.ID,
                                 "http://www.sat.dundee.ac.uk/xrit/135.0W/GOES/",
                                 "_GOES15_4_S2.jpeg"
                                 ),
@@ -236,7 +239,7 @@ if __name__ == '__main__':
     for satellite in satellite_list:
         print "Satellite file: ", satellite.filename
         satellite.download_image()
-        img = project(satellite)
+        img = satellite.project()
         if args.debug: saveImage(img[0], 
                                  os.path.join(tempdir, "test" + `i` + ".jpeg"), 
                                  overlays=True)
