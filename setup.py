529c1c2c45e623f1a5e305acff2e13725725f24b
Joachim Herb <Joachim.Herb@gmx.de>
2013-12-06 00:42:35 +0100

    Do not overwrite existing config file or delete it during uninstall

--- setup.py
+++ setup.py
@@ -6,16 +6,40 @@ setup(
     version='0.1.0',
     packages=['cloudmap', ],
     license='LICENSE',
-    description='Python script to create a cloud map for xplanet using satellite images from the Dundee Satellite Receiving Station, Dundee University, UK',
+    description='Create a cloud map for xplanet using satellite images from the Dundee Satellite Receiving Station, Dundee University, UK',
     long_description=open('README.md').read(),
     author='Joachim Herb',
     author_email='Joachim.Herb@gmx.de',
     install_requires=['pyresample', 'numpy', 'scipy', 'requests', 'datetime', 
                       'argparse', 'ConfigParser', 'PIL'],
-    data_files=[(os.path.expanduser('~/.CreateCloudMap'), ['cfg/CreateCloudMap.ini', ]),],
     entry_points={
         'console_scripts': [
             'create_map = cloudmap.create_map:main',
         ]
     }
 )
+
+def mkdir_p(path):
+    import errno
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
+
+from sys import argv
+try:
+    if argv[1] == 'install':
+        from shutil import copyfile
+        src = os.path.join('cfg', 'CreateCloudMap.ini')
+        dstdir = os.path.expanduser('~/.CreateCloudMap')
+        dstfile = os.path.join(dstdir, 'CreateCloudMap.ini')
+        updatefile = os.path.join(dstdir, 'CreateCloudMap.ini.new')
+        mkdir_p(dstdir)
+        if not os.path.exists(dstfile):
+            copyfile(src, dstfile)
+        else:
+            copyfile(src, updatefile)
+            
+except IndexError: pass
