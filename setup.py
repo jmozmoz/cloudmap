0a0e3e6b8829d2fe0663a1a680dd2acd7d1ded78
Joachim Herb <Joachim.Herb@gmx.de>
2013-12-05 01:00:33 +0100

    Preparations for release as package

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+ 
+setup(
+    name='CreateCloudMap',
+    version='0.1.0',
+    packages=['cloudmap', ],
+    license='LICENSE',
+    description='Python script to create a cloud map for xplanet using satellite images from the Dundee Satellite Receiving Station, Dundee University, UK',
+    long_description=open('README.md').read(),
+    author='Joachim Herb',
+    author_email='Joachim.Herb@gmx.de',
+    install_requires=['pyresample', 'numpy', 'scipy', 'requests', 'datetime', 
+                      'argparse', 'ConfigParser', 'PIL', 'matplotlib', 'Basemap'],
+    entry_points={
+        'console_scripts': [
+            'create_map = cloudmap.create_map:main',
+        ]
+    }
+)
