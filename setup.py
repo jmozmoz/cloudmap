9ea11db27e1883f4753c4c4b7d86c078e6293f29
jmozmoz <Joachim.Herb@gmx.de>
2015-06-27 16:25:14 +0200

    Compatibility with Python 2/3. Use shields.io also on pypi.python.org

--- setup.py
+++ setup.py
@@ -85,7 +85,9 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
