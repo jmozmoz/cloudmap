cf0f4065e1f5e26bf1c406b09aa14759dce7c96a
jmozmoz <Joachim.Herb@gmx.de>
2015-12-18 23:31:51 +0100

    Add compatiblity classifier for Python 3.5 to setup.py

--- setup.py
+++ setup.py
@@ -90,6 +90,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
